fde4465c60e328ce6057a1a1dcd42503e25582de
Evan Holloway <eholloway00@gmail.com>
2024-04-25 11:05:06 -0700

    Fixed secrets and Updated seedData

--- Snipper.server/Controllers/UserController.cs
+++ Snipper.server/Controllers/UserController.cs
@@ -1,55 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Snipper.server.Models;
+﻿//using Microsoft.AspNetCore.Mvc;
+//using Snipper.server.Models;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+//// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace Snipper.server.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UserController : ControllerBase
-    {
-        public static List<User> users = new List<User>();
+//namespace Snipper.server.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class UserController : ControllerBase
+//    {
+//        public static List<User> users = new List<User>();
 
-        public static long userId = users.Count;
+//        public static long userId = users.Count;
 
-        private readonly ILogger<UserController> _logger;
+//        private readonly ILogger<UserController> _logger;
 
-        public UserController(ILogger<UserController> logger)
-        {
-            _logger = logger;
-        }
+//        public UserController(ILogger<UserController> logger)
+//        {
+//            _logger = logger;
+//        }
 
-        // GET: api/<UserController>
-        [HttpGet]
-        public ActionResult<List<User>> GetUsers(string username, string password)
-        {
-            return new string[] { "value1", "value2" };
-        }
+//        // GET: api/<UserController>
+//        [HttpGet]
+//        public ActionResult<List<User>> GetUsers(string username, string password)
+//        {
+//            return new string[] { "value1", "value2" };
+//        }
 
-        // GET api/<UserController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+//        // GET api/<UserController>/5
+//        [HttpGet("{id}")]
+//        public string Get(int id)
+//        {
+//            return "value";
+//        }
 
-        // POST api/<UserController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
+//        // POST api/<UserController>
+//        [HttpPost]
+//        public void Post([FromBody] string value)
+//        {
+//        }
 
-        // PUT api/<UserController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+//        // PUT api/<UserController>/5
+//        [HttpPut("{id}")]
+//        public void Put(int id, [FromBody] string value)
+//        {
+//        }
 
-        // DELETE api/<UserController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
-    }
-}
+//        // DELETE api/<UserController>/5
+//        [HttpDelete("{id}")]
+//        public void Delete(int id)
+//        {
+//        }
+//    }
+//}
